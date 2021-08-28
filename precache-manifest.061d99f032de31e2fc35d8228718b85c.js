e25e0c802dc8014c82a1052243e826722ab3a684
Andy Chen <dershan@chendexuandeMacBook-Pro.local>
2021-08-28 15:19:42 -0400

    Updates

---
+++ precache-manifest.061d99f032de31e2fc35d8228718b85c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73921bbe3dad3ef6aceef7a16f840cc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50bb050ec80243d93c17",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "80ece100983d1ff008a3",
+    "url": "/static/js/2.a0971961.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a0971961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50bb050ec80243d93c17",
+    "url": "/static/js/main.f9d0be25.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
